502e086aff5f49e3f76a2a9b3e5e70205b4765da
smash <smash@DESKTOP-MJASLIS>
2022-04-06 16:47:55 +0300

    Add project files.

---
+++ Net_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Net_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Miroslav!");
+            Console.Read();
+        }
+    }
+}
