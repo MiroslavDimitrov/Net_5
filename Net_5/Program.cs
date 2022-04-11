8f3f60813ac2e888953d5cc0a9748e1c0d5c8cf8
Miroslav Todorov <smash.all22@outlook.com>
2022-04-11 10:19:26 +0300

    #work/study_01 - search character

--- Net_5/Program.cs
+++ Net_5/Program.cs
@@ -10,6 +10,12 @@ namespace Net_5
             Console.WriteLine("Pleace enter your name and press enter : ");
             input = Console.ReadLine();
             Console.WriteLine(input);
+
+            Console.WriteLine("Enter a string here : ");
+            Console.WriteLine("Enter a character to search : ");
+            char charInput = Console.ReadLine()[0];//read the character from the string
+            int index = input.IndexOf(charInput);//gets the index of he character from string
+            Console.WriteLine("Index of character {0} in string is {1}", charInput, index);
         }
     }
 }
